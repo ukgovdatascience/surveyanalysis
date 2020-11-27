619e4c2f08a835e61e259ef3d08175c53d8f5983
avisionh <a_vision@hotmail.co.uk>
2020-11-27 21:43:28 +0000

    Handle duplicated rows
    This is so we can pivot wider without creating lists.
    
    Also, export to csv for sharing.

--- src/make_data/01_link_data.R
+++ src/make_data/01_link_data.R
@@ -4,6 +4,29 @@ library(readxl)
 library(httr)
 library(dplyr)
 
+# from Duncan - function for importing
+read_responses <- function(file) {
+  read_csv(file, col_types = cols(.default = col_character())) %>%
+    pivot_longer(-c(pupil_id, pupil_impacted_id, measurement_date),
+      names_to = "question", values_to = "response"
+    ) %>%
+    # Deal with duplicated questions.
+    # Three pupils in questionnaire 186 have a set of columns to themselves, so
+    # we merge them back in by:
+    #
+    # 1. filtering out everyone's blank responses.  That means that most pupils
+    #    only have responses to the first instance of each question, and the
+    #    three affected pupils only have responses to the second instance of
+    #    each question.
+    # 2. dropping the suffix that readr automatically adds to the second
+    #   instance of each question.  Now all pupils are the same.
+    #
+    # instance of each column.
+    filter(!is.na(response)) %>%
+    mutate(question = str_remove(question, "_\\d+$"))
+}
+
+
 # get list of all files
 file_main <- drive_get(path = as_id(x = "https://drive.google.com/file/d/1PS9xQIP_O048rGb-uvwPonyrV_3CCYc4/view?usp=sharing"))
 files <- drive_ls(
@@ -31,12 +54,14 @@ sheet_questionnaires <- read_excel(
 )
 
 # rename columns
-sheet_questions <- rename(
-  .data = sheet_questions,
-  measure = `...1`,
-  questionnaire = `...2`,
-  question = `...3`
-)
+sheet_questions <- sheet_questions %>%
+  rename(
+    .data = sheet_questions,
+    measure = `...1`,
+    questionnaire = `...2`,
+    question = `...3`
+  ) %>%
+  fill(measure, .direction = "down")
 sheet_questionnaires <- select(.data = sheet_questionnaires, pupil_id:dSEND)
 
 # import each csv from gdrive
@@ -45,15 +70,15 @@ j <- 1
 
 for (i in files$drive_resource) {
   link <- i$webContentLink
-  df <- read_csv(file = link)
+  df <- read_responses(file = link)
   # store in list
   list_df[[j]] <- df
   j <- j + 1
 }
 
-# name list for easy access of each df
-names(list_df) <- files$name
+responses <- map_dfr(.x = list_df, .f = rbind) %>%
+  mutate(measurement_date = parse_date(x = measurement_date, format = "%d/%m/%Y"))
 
 # clear environment
 unlink(tf)
-rm(df, file_main, files, i, j, link, tf)
+rm(df, file_main, files, i, j, link, list_df, tf)
--- src/make_data/02_link_data.R
+++ src/make_data/02_link_data.R
@@ -2,20 +2,43 @@ source("src/make_data/01_link_data.R")
 
 library(dplyr)
 library(tidyr)
-library(purrr)
+library(stringr)
 
 
-# join so in format requested
-df_step <- list_df[[1]]
-for (i in 1:(length(list_df) - 1)) {
-  df_step <- df_step %>%
-    full_join(y = list_df[[i + 1]], by = c("pupil_id", "pupil_impacted_id", "measurement_date"))
-}
+# get questionnaire info in to use as column names later
+responses <- responses %>%
+  left_join(y = sheet_questions, by = "question") %>%
+  mutate(qq = paste0(questionnaire, " - ", question)) %>%
+  # remove duplicates
+  distinct()
 
-# we have repeated columns that exist in the source csv files
-# however, there are three valid responses in the repeated columns that are not in the
-# non-repeated columns
-df <- df_step %>%
-  select(!ends_with(match = "_1")) %>%
-  rename_at(.vars = vars(sheet_questions$question), .funs = list(sheet_questions$questionnaire))
-df <- rename_at(.data)
+
+# see if have unique combo of rows so can pivot_wider safely
+responses %>%
+  group_by(pupil_id, pupil_impacted_id, measurement_date, qq) %>%
+  summarise(count = n()) %>%
+  filter(count > 1)
+# have duplicates, here's an example
+responses %>%
+  filter(pupil_id == "100165", measurement_date == "2020-09-09", qq == "207_10 - I have felt like I have missed important school work")
+
+# partition these duplicate responses with a row number; allocation of this is random
+# this seems the best we can do
+responses <- responses %>%
+  select(pupil_id:measurement_date, qq, response) %>%
+  group_by(pupil_id, pupil_impacted_id, measurement_date, qq) %>%
+  mutate(rank = row_number()) %>%
+  arrange(pupil_id, pupil_impacted_id, measurement_date, qq)
+
+# isolate unique records
+responses_dedupe <- filter(.data = responses, rank == 1)
+
+# pivot wider for ImpactEd's purposes
+df_output <- responses_dedupe %>%
+  pivot_wider(
+    id_cols = c(pupil_id, pupil_impacted_id, measurement_date),
+    names_from = "qq",
+    values_from = "response"
+  ) %>%
+  arrange(pupil_id, measurement_date)
+write_csv(x = df_output, file = "data/processed/questionnaires_linked.csv")
