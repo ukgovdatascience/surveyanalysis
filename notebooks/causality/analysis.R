7b9fe6565195ea1bade43a6d20cfc888a4df0d18
avisionh <a_vision@hotmail.co.uk>
2021-01-05 12:15:40 +0000

    Compute summary mode for each dependent variable
    This is so we have a single target variable to aim for.

--- notebooks/causality/analysis.R
+++ notebooks/causality/analysis.R
@@ -1,5 +1,8 @@
 library(readr)
 library(dplyr)
+library(stringr)
+
+source("src/utils/get_mode.R")
 
 # ensure you run `direnv allow` in terminal
 readRenviron(path = ".env")
@@ -14,6 +17,34 @@ for (i in seq_len(length.out = length(x = df_files))) {
 }
 names(list_dfs) <- df_names
 
+rm(df_names, df_files, i)
+
 # Is there any way of understanding which factors
 # (contextual survey responses or demographics)
 # are most predictive of the wellbeing or learning scores
+
+# what are our wellbeing and learning variables?
+filter_dv <- list_dfs[["df_questions"]] %>%
+  filter(str_detect(string = measure, pattern = "(?i)wellbeing|learn")) %>%
+  distinct(questionnaire) %>%
+  pull()
+
+# get single outcome variable for our dependent variables of wellbeing and learning
+# to make modelling easier
+# do this by using mode since are dealing with categorical variables
+
+# likert scale so 5
+n_lvls <- 5
+factor_lvls <- c(seq_len(length.out = n_lvls), NA)
+
+df_dv <- list_dfs[["df_responses"]] %>%
+  select(contains(match = c("pupil_id", "measurement_date", filter_dv))) %>%
+  # convert to factors
+  mutate(across(.cols = contains(match = filter_dv), .fns = ~ factor(x = .x, levels = factor_lvls, ordered = TRUE))) %>%
+  rowwise() %>%
+  # compute mode for each record
+  mutate(
+    mode_wellbeing = get_mode(x = c_across(cols = starts_with(match = "185"))),
+    mode_contextlearningaccess = get_mode(x = c_across(cols = starts_with(match = "187"))),
+    mode_remotelearn = get_mode(x = c_across(cols = starts_with(match = "188")))
+  )
